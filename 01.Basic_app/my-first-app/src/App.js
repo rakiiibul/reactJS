95fcd5c74b10337f7c002ead9a2e296f29c43132
unknown <therakiiibul@outlook.com>
2021-04-19 23:51:53 +0600

    change

--- 01.Basic_app/my-first-app/src/App.js
+++ 01.Basic_app/my-first-app/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hi,I'm a react Application</h1>
+       <h1>Hi,I'm a react Developer</h1>
+       <Person></Person>
+       <Person></Person>
+       <Person></Person>
+       <Person></Person>
+       <Person></Person>
+      
+
       </div>
     );
   }
---
+++ 01.Basic_app/my-first-app/src/Person/Person.js
@@ -0,0 +1,7 @@
+
+import React from 'react'; 
+const person=()=>{
+    return <p>I am Raikibul Hasan and I am {Math.floor(Math.random()*30)} years old!</p>
+
+};
+export default person;
